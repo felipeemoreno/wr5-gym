038a1c92bea8e8f2d58e216fceb4c229df2aa657
felipemoreno <contato@wr5solutions.com.br>
2024-10-24 09:24:09 -0300

    added: sentry

--- App.tsx
+++ App.tsx
@@ -1,9 +1,18 @@
-import { StatusBar, StyleSheet, Text, View } from 'react-native';
+import { Button, StatusBar, StyleSheet, Text, View } from 'react-native';
 import {
 useFonts,
 Roboto_400Regular,
 Roboto_700Bold
 } from "@expo-google-fonts/roboto";
+import * as Sentry from '@sentry/react-native';
+
+Sentry.init({
+  dsn: 'https://ecda728938f0183ab0891c2fd347f654@o4503938919563264.ingest.us.sentry.io/4508177666080768',
+  defaultIntegrations: false
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // enableSpotlight: __DEV__,
+});
 
 export default function App() {
 
