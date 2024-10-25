40a2b7c2e9fca452d655f9b4321ac2900095318b
felipemoreno <contato@wr5solutions.com.br>
2024-10-25 09:07:30 -0300

    added: Loading Component

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,8 +1,4 @@
 import { Center, Spinner } from '@gluestack-ui/themed';
-import React from 'react';
-import { View } from 'react-native';
-
-// import { Container } from './styles';
 
 const Loading: React.FC = () => {
   return (
