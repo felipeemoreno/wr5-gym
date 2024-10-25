9470ebdede6019f954ac20ba1b617817a004f87b
felipemoreno <contato@wr5solutions.com.br>
2024-10-25 09:16:47 -0300

    added: svg transformer

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,26 @@
+const { getDefaultConfig } = require("expo/metro-config");
 const { getSentryExpoConfig } = require("@sentry/react-native/metro");
 
-const config = getSentryExpoConfig(__dirname);
+module.exports = (() => {
+  const configExpo = getDefaultConfig(__dirname);
+  const configSentry = getSentryExpoConfig(__dirname);
 
-module.exports = config;
+  const { transformer, resolver } = configExpo;
+
+  configExpo.transformer = {
+    ...transformer,
+    babelTransformerPath: require.resolve("react-native-svg-transformer/expo")
+  };
+  configExpo.resolver = {
+    ...resolver,
+    assetExts: resolver.assetExts.filter((ext) => ext !== "svg"),
+    sourceExts: [...resolver.sourceExts, "svg"]
+  };
+
+  const config = {
+    ...configExpo,
+    ...configSentry
+  };
+
+  return config;
+})();
