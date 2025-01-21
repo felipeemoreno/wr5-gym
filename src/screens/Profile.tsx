7cfe0263d33e3b76f23cbbddf33a4e59d1f1dda7
Felipe Estevão Moreno <contato@wr5solutions.com.br>
2025-01-21 16:22:16 -0300

    added: toast

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,25 +1,74 @@
-import { Center, Heading, Text, VStack } from "@gluestack-ui/themed"
+import { Center, Heading, Text, useToast, VStack } from "@gluestack-ui/themed"
+import * as ImagePicker from "expo-image-picker"
+import * as FileSystem from "expo-file-system"
 import { ScrollView, TouchableOpacity } from "react-native"
 import { Input } from "@components/Input"
 
+import { ToastMessage } from "@components/ToastMessage"
 import { ScreenHeader } from "@components/ScreenHeader"
 import UserPhoto from "@components/UserPhoto"
 import Button from "@components/Button"
+import { useState } from "react"
 
 export const Profile: React.FC = () => {
+  const [userPhoto, setUserPhoto] = useState<string>(
+    "https://github.com/felipeemoreno.png",
+  )
+
+  const toast = useToast()
+
+  const handleUserPhotoSelect = async () => {
+    try {
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ["images"],
+        quality: 1,
+        aspect: [4, 4],
+        allowsEditing: true,
+      })
+
+      if (photoSelected.canceled) return
+
+      const photoUri = photoSelected.assets[0].uri
+
+      if (photoUri) {
+        const photoInfo = (await FileSystem.getInfoAsync(photoUri)) as {
+          size: number
+        }
+
+        if (photoInfo.size / 1024 / 1024 > 5) {
+          return toast.show({
+            placement: "top",
+            render: ({ id }) => (
+              <ToastMessage
+                id={id}
+                action="error"
+                title="Imagem é muito grande"
+                description="Escolha uma imagem com até 5MB"
+                onClose={() => toast.close(id)}
+              />
+            ),
+          })
+        }
+
+        setUserPhoto(photoUri)
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <VStack flex={1}>
       <ScreenHeader title="Perfil" />
-
       <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
         <Center mt="$6" px="$10">
           <UserPhoto
-            source={{ uri: "https://github.com/felipeemoreno.png" }}
+            source={{ uri: userPhoto }}
             alt="Foto do usuário"
             size="xl"
           />
 
-          <TouchableOpacity>
+          <TouchableOpacity onPress={handleUserPhotoSelect}>
             <Text
               color="$green500"
               fontFamily="$heading"
